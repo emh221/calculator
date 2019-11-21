bdd52c2c28d2c9f36a1a3021a6f1c5c0fab9cd10
em221 <emhd221@gmail.com>
2019-11-21 09:30:24 -0800

    add user interface and front end logic to calculator project.

--- js/scripts.js
+++ js/scripts.js
@@ -2,52 +2,48 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2));
-
 var subtract = function(number1, number2) {
   return number1 - number2;
 };
-
-var number1 = parseInt(prompt("Enter a number:"));
-
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(subtract(number1, number2));
 
 var multiply = function(number1, number2) {
   return number1 * number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(multiply(number1, number2));
-
 var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
+$(document).ready(function() {
+  $("form#add").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
 
-var number2 = parseInt(prompt("Enter another number:"));
+  $("form#subtract").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#output2").text(result);
+  });
 
-alert(divide(number1, number2));
+  $("form#multiply").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#output3").text(result);
+  });
 
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
-
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
-
-var farenheit = parseInt(prompt("What is the temperature in Farenheit?"));
-
-var celsius = subtract(farenheit, 32);
-
-alert(divide(celsius,1.8))
+  $("form#divide").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+    $("#output4").text(result);
+  });
+});
